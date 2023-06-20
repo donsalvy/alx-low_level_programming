c0f234e7eac51ca77b9bafec10f98795700bf97f
donsalvy <donsalvy2007@gmail.com>
2023-06-20 16:44:24 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 #include "main.h"
-/**
-* main - print _putchar
-* is written salvation
-* Return: 0 which is a success
-*/
+	/**
+	 * main - print _putchar
+	 * is written by salvation
+	 * Return: 0 which means success
+	 */
 
 
-int print_putchar(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
+
